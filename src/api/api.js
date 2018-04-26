cadbe972b6bffd3ea3a346ad34a3dac7967a190f
zhugeyifan <zhugeyifan@acoinfo.com>
2018-04-26 16:51:47 +0800

    enable mock

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 let base = '';
 
-//export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
-export const requestLogin = params => { return axios.post(`action/webLoginAction`, params).then(res => res.data); };
+export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
+//export const requestLogin = params => { return axios.post(`action/webLoginAction`, params).then(res => res.data); };
 
 export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
 
@@ -19,9 +19,9 @@ export const addUser = params => { return axios.get(`${base}/user/add`, { params
 
 export const getSystem = params => { return axios.get(`${base}/system`, { params: params }); };
 
-export const dialUp = params => { return axios.post(`action/udialHandleAction`,params ); };
-//export const dialUp = params => { return axios.post(`${base}/api/dialUp`, params); };
+//export const dialUp = params => { return axios.post(`action/udialHandleAction`,params ); };
+export const dialUp = params => { return axios.post(`${base}/api/dialUp`, params); };
 //export const dialUp = params  => {return axios.create({url:'${base}/api/dialUp', method:'post', headers:'Content-Type:text/javascript', params:params}).then(res => res.data)}
 
-//export const getPorts = data => { return axios.post(`${base}/api/ports`, { params: data }); };
-export const getPorts = data => { return axios.post(`action/interfaceMonitorAction`); };
+export const getPorts = data => { return axios.post(`${base}/api/ports`, { params: data }); };
+//export const getPorts = data => { return axios.post(`action/interfaceMonitorAction`); };
